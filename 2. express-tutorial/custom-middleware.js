4ef19215fcbd40defe26785a2812bd3bd4f5d619
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-01-16 15:54:23 +0530

    more module added

--- 2. express-tutorial/custom-middleware.js
+++ 2. express-tutorial/custom-middleware.js
@@ -4,12 +4,12 @@ const app = express();
 const requestTimestampLogger = (req, res, next) => {
   const timeStamp = new Date().toISOString();
 
-  console.log(`${timeStamp} from ${req.method} to ${req.url}`);
-  next();
-};
+  console.log(`${timeStamp} from ${req.method} to  ${req.url}` );
+  next()
+  
+}
 
 app.use(requestTimestampLogger);
-
 app.get("/", (req, res) => {
   res.send("Home page");
 });
